f830bc516dee60ee07380f26c92f953f6eda4b34
Grace Tang <wxm0323@gmail.com>
2025-11-04 14:37:14 -0600

    Added Firebase authentication dependencies in Gradle builds

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    // Google Services plugin for Firebase
+    id("com.google.gms.google-services") version "4.4.4" apply false
 }
 
 buildscript {
