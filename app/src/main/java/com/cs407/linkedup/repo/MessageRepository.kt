78bd9dc258a1cbc8dc00334853f3deef0b4ac35a
ArturSobol1 <artur.r.sobol@gmail.com>
2025-11-20 16:11:29 -0600

    added some message outlining

---
+++ app/src/main/java/com/cs407/linkedup/repo/MessageRepository.kt
@@ -0,0 +1,22 @@
+package com.cs407.linkedup.repo
+
+
+
+//structure in database for messages
+// main database:
+// ---|users:
+// ------| ... all user info
+// ---| conversations
+// ------| covnersation by id
+// ---------| participants: [userA, userB, ...]
+// ---------| ... rest of message data like time staps etc.
+// ---------| messages:
+// ------------| ... all mesage data
+class MessageRepository {
+    fun sendMessage(){
+
+    }
+    fun getMessageList(){
+
+    }
+}
